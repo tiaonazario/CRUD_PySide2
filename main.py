1a16eb9493a51aac7c969fea0f99ee00fca301fb
Tião Nazário <tiaogogo@gmail.com>
2021-06-02 22:15:23 -0300

    janela

---
+++ main.py
@@ -0,0 +1,22 @@
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+import sys
+
+
+class JanelaPrincipal(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(JanelaPrincipal, self).__init__(args, **kwargs)
+        self.setWindowTitle("Controle de Estoque")
+        self.setMinimumSize(600, 400)
+
+    def abrir(self):
+        self.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    j = JanelaPrincipal()
+    j.abrir()
+
+    sys.exit(app.exec_())
