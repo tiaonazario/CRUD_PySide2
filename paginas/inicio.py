d4fba5403d80f468264373f2a478a15cd5657706
Tião Nazário <tiaogogo@gmail.com>
2021-06-06 15:15:27 -0300

    outro

--- paginas/inicio.py
+++ paginas/inicio.py
@@ -5,7 +5,8 @@ import sys
 class INICIO(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(INICIO, self).__init__(*args, **kwargs)
-        self.setMinimumSize(600, 400)
+        self.CentroInicio = QWidget(self)
+        self.LGInicio = QGridLayout(self)
 
 
 if __name__ == "__main__":
