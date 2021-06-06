1c85be322fa49ff377f54089b963c2eb421fbd16
Tião Nazário <tiaogogo@gmail.com>
2021-06-06 10:12:41 -0300

    novo

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from PySide2.QtWidgets import *
 import sys
 
 
-class MainWindow(QMainWindow):
+class JANELA(QMainWindow):
     def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-        self.setWindowTitle("Controle de Estoque")
-        self.setMinimumSize(600, 400)
+        super(JANELA, self).__init__(*args, **kwargs)
+        self.setWindowTitle("Controle de Estoque")  # titulo da janela
+        self.setMinimumSize(600, 400)  # as menores dimensoes aceitavel pelo programa
 
     def abrir(self):
         self.show()
@@ -14,7 +14,7 @@ class MainWindow(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    j = MainWindow()
+    j = JANELA()
     j.abrir()
 
     sys.exit(app.exec_())
